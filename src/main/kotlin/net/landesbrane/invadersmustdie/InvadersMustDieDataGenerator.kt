cfd1069f826f5bfa3bacf74a6fc77f3cbb9d2623
PMAlekseev <pmalekseev@mai.education>
2024-10-07 10:23:06 +0300

    Mod created

---
+++ src/main/kotlin/net/landesbrane/invadersmustdie/InvadersMustDieDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.landesbrane.invadersmustdie
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object InvadersMustDieDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
